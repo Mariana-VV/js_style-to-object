4f8f9edbb8282dbe34d3999bf2b8b42745cb8c56
Mariana Trinko <marjanavv@gmail.com>
2024-10-18 21:04:02 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,36 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceStyle = sourceString.split(';');
+  const style = {};
+
+  Object.create(style);
+
+  sourceStyle.forEach((source) => {
+    const data = source.split(':');
+
+    if (data[0].trim()) {
+      const key = data[0].trim();
+      const value = data[1];
+
+      const s = value;
+      let result;
+
+      if (!s.includes(',')) {
+        result = typeof s === 'string' ? s.replace(/[\t\n]/g, '').trim() : null;
+        style[key] = result;
+      } else {
+        style[key] =
+          typeof s === 'string' ? s.replace(/[\t\n]/g, '\n').trim() : null;
+      }
+    }
+  });
+
+  return style;
 }
+
+// function isLetter(str) {
+//   return str.length === 1 && str.match(/[a-z]/i);
+// }
 
 module.exports = convertToObject;
